2ae47cba8461eea00f92a4d6f857b5153f747851
test <test@test.com>
2025-01-26 20:38:22 -0800

    add button

--- main.cpp
+++ main.cpp
@@ -48,7 +48,44 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow
 }
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    static HWND hButton1, hButton2;
     switch (uMsg) {
+
+    case WM_CREATE:
+        hButton1 = CreateWindow(
+            L"BUTTON",
+            L"click 1",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            50, 50, 100, 30,
+            hwnd,
+            (HMENU)1,
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            NULL
+        );
+
+        hButton2 = CreateWindow(
+            L"BUTTON",
+            L"click 2",
+            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
+            160, 50, 100, 30,
+            hwnd,
+            (HMENU)2,
+            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
+            NULL
+        );
+        break;
+    
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) { // Check if the button was clicked
+        case 1:
+            MessageBox(hwnd, L"You clicked button 1!", L"Button 1 Clicked", MB_OK);
+            break;
+        case 2:
+            MessageBox(hwnd, L"You clicked button 2!", L"Button 2 Clicked", MB_OK);
+            break;
+        }
+        break;
+
     case WM_DESTROY:
         PostQuitMessage(0);
         return 0;

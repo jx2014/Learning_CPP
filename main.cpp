b04ba3e0620c509eb3c25f1038ec1806b06a8792
desktop <jx2014@users.noreply.github.com>
2025-01-28 22:12:53 -0800

    padsf

--- main.cpp
+++ main.cpp
@@ -1,102 +1,77 @@
-#define UNICODE
-#define _UNICODE
 #include <windows.h>
 
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LPCTSTR g_szClassName = TEXT("myWindowClass");
 
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
-    // Register the window class
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
+// Step 4: the Window Procedure
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_CLOSE:
+            DestroyWindow(hwnd);
+        break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
 
-    WNDCLASS wc = {};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
+{
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
 
-    RegisterClass(&wc);
+    //Step 1: Registering the Window Class
+    wc.cbSize        = sizeof(WNDCLASSEX);
+    wc.style         = 0;
+    wc.lpfnWndProc   = WndProc;
+    wc.cbClsExtra    = 0;
+    wc.cbWndExtra    = 0;
+    wc.hInstance     = hInstance;
+    wc.hIcon         = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor       = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW+1);
+    wc.lpszMenuName  = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm       = LoadIcon(NULL, IDI_APPLICATION);
 
-    // Create the window
-    HWND hwnd = CreateWindowEx(
-        0,                              // Optional window styles
-        CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
+    if(!RegisterClassEx(&wc))
+    {
+        MessageBox(NULL, TEXT("Window Registration Failed!"), TEXT("Error!"),
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
+    }
 
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+    // Step 2: Creating the Window
+    hwnd = CreateWindowEx(
+        0, //WS_EX_CLIENTEDGE,
+        g_szClassName,
+        TEXT("The title of my window"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 340, 220,
+        // parent window handle,   menu handle,  application instance handle,   pointer to winodw creation data.
+        NULL, NULL, hInstance, NULL);
 
-        NULL,       // Parent window
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        NULL        // Additional application data
-    );
-
-    if (hwnd == NULL) {
+    if(hwnd == NULL)
+    {
+        MessageBox(NULL, TEXT("Window Creation Failed!"), TEXT("Error!"),
+            MB_ICONEXCLAMATION | MB_OK);
         return 0;
     }
 
     ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
 
-    // Run the message loop
-    MSG msg = {};
-    while (GetMessage(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
+    // Step 3: The Message Loop
+    while(GetMessage(&Msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&Msg);
+        DispatchMessage(&Msg);
     }
-
-    return 0;
-}
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    static HWND hButton1, hButton2;
-    switch (uMsg) {
-
-    case WM_CREATE:
-        hButton1 = CreateWindow(
-            L"BUTTON",
-            L"click 1",
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            50, 50, 100, 30,
-            hwnd,
-            (HMENU)1,
-            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-            NULL
-        );
-
-        hButton2 = CreateWindow(
-            L"BUTTON",
-            L"click 2",
-            WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,
-            160, 50, 100, 30,
-            hwnd,
-            (HMENU)2,
-            (HINSTANCE)GetWindowLongPtr(hwnd, GWLP_HINSTANCE),
-            NULL
-        );
-        break;
-    
-    case WM_COMMAND:
-        switch (LOWORD(wParam)) { // Check if the button was clicked
-        case 1:
-            MessageBox(hwnd, L"You clicked button 1!", L"Button 1 Clicked", MB_OK);
-            break;
-        case 2:
-            MessageBox(hwnd, L"You clicked button 2!", L"Button 2 Clicked", MB_OK);
-            break;
-        }
-        break;
-
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        return 0;
-
-    case WM_PAINT: {
-        PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hwnd, &ps);
-        FillRect(hdc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
-        EndPaint(hwnd, &ps);
-    }
-        return 0;
-    }
-    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    return Msg.wParam;
 }

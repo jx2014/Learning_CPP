ce6b359fa2ac6bd1f46317d872fee7aa79084863
desktop <jx2014@users.noreply.github.com>
2025-05-24 15:23:43 -0700

    relearn c++

--- main.cpp
+++ main.cpp
@@ -1,58 +1,18 @@
-#define UNICODE
-#define _UNICODE
-#include <windows.h>
-#include <wchar.h>
-#include <commctrl.h>
-
-
-// https://learn.microsoft.com/en-us/windows/win32/learnwin32/painting-the-window
-
-template <class DERIVED_TYPE>
-
-LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
-    // Register the window class
-    const wchar_t CLASS_NAME[] = L"Sample Window Class";
-    StateInfo *pState = new (std::nothrow) StateInfo;
-
-    WNDCLASSW wc = {0};
-    wc.lpfnWndProc = WindowProc;
-    wc.hInstance = hInstance;
-    wc.lpszClassName = CLASS_NAME;
-
-    RegisterClassW(&wc);
-
-    // Create the window
-    HWND hwnd = CreateWindowExW(
-        0,                              // Optional window styles
-        CLASS_NAME,                     // Window class
-        L"Learn to Program Windows",    // Window text
-        WS_OVERLAPPEDWINDOW,            // Window style
-
-        // Size and position
-        CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-
-        NULL,       // Parent window
-        NULL,       // Menu
-        hInstance,  // Instance handle
-        pState,     // Additional application data
-    );
-
-    if (hwnd == NULL) {
-        return 0;
-    }
-
-    ShowWindow(hwnd, nCmdShow);
-
-    // Run the message loop
-    MSG msg = {};
-    while (GetMessageW(&msg, NULL, 0, 0)) {
-        TranslateMessage(&msg);
-        DispatchMessageW(&msg);
+#include <iostream>
+#include "Sales_item.h"
+int main()
+{
+    // 1.20 Use it to write a program that reads a set of book sales transactions, writing each transaction to the standard output.
+    Sales_item item;
+    while (true) {
+        std::cin >> item;
+        if (item.isbn() == "") {
+            break;
+        }
+        std::cout << item << std::endl;
     }
 
     return 0;
 }
 
-
+// https://cpp-primer.pages.dev/book/015-1.5._introducing_classes.html

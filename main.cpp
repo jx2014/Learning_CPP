a782251cdf0c89c18df7f675c65b328c837dc5ae
desktop <jx2014@users.noreply.github.com>
2025-06-28 16:08:38 -0700

    vector string

--- main.cpp
+++ main.cpp
@@ -2,11 +2,13 @@
 #include <string>
 #include <iomanip> // for std::setw()
 #include <cctype>
+#include <vector>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
+using std::vector;
 
 int main() {
     // int i;
@@ -61,16 +63,114 @@ int main() {
     // cout << "Your hex number is: " << result << endl;
 
     // exercise 3.10 remove all punctuations
-    string line;
-    auto len = line.size();
+    // string line;
+    // auto len = line.size();
 
-    while (getline(cin, line)) {                
-        for (auto c : line) {       // for every char in s
-            if (!ispunct(c))     // if the character is punctuation
-                cout << c;            
-        }
-        cout << endl;
-    }
+    // while (getline(cin, line)) {                
+    //     for (auto c : line) {       // for every char in s
+    //         if (!ispunct(c))     // if the character is punctuation
+    //             cout << c;            
+    //     }
+    //     cout << endl;
+    // }
+
+// 3.3 Library vector Type
+    // vector<int> v2(10, 42);     // 10 elements, each has value 42;
+    // vector<int> v4{10};         // one element of value of 10;
+    // vector<int> v5{10, 42};     // two elements, value of 10, and 42;
+    // vector<string> v6{10};      // ten elements of null
+    // vector<string> v7{10, "hi"}; // ten elements of "hi"
+
+    // vector<int> v(10);
+    // cout << v.size() << endl;
+    // int n = 0;
+    // for (auto &i : v) {
+    //     n++;
+    //     i = n * n;
+    //     cout << i << " ";
+    // }
+    // cout << v[0] << " " << v[10] << endl;
+
+    
+    // Exercise 3.17: Read a sequence of words from cin and store the values a vector.
+    // After you’ve read all the words, process the vector and change each word to uppercase. 
+    // Print the transformed elements, eight words to a line.
+
+    // string word;
+    // vector<string> text;
+
+    // cout << "Enter some sentences: ";
+    // while (cin >> word) {
+    //     text.push_back(word);
+    //     if (cin.peek() == '\n') {
+    //         cin.get();
+    //         break;
+    //     }
+
+    // }
+
+    // for (auto &w : text) {
+    //     for (auto &l : w) {
+    //         l = toupper(l);
+    //     }
+    // }
+
+    // uint8_t n = 0;
+    // for (auto &w : text) {
+    //     cout << w << " ";
+    //     n++;
+    //     if (n == 7) {
+    //         n = 0;
+    //         cout << endl;
+    //     }
+    // }
+
+    // Exercise 3.18
+    // vector<int> ivec;
+    // ivec[0] = 42; // incorrect, ivec size is 0
+    // ivec.push_back(42); // one way to fix it.
+    
+    // Exercise 3.19: 
+    //List three ways to define a vector and give it ten elements, each with the value 42. 
+    // Indicate whether there is a preferred way to do so and why.
+    // vector<int> ivec(10, 42);   // preferred
+    // vector<int> ivec2;           // init w/ empty vector
+    // for (int i = 0; i < 10; i++)    // use loop, convoluted.
+    //     ivec2.push_back(42);
+    // vector<int> ivec3(10);
+
+    // Exercise 3.20: 
+    // Read a set of integers into a vector. Print the sum of each pair of adjacent elements. 
+    // Change your program so that it prints the sum of the first and last elements, 
+    // followed by the sum of the second and second-to-last, and so on.
+    // vector<int> ivec;
+    // int num;
+
+    // cout << "Enter some numbers: ";
+    // while (cin >> num) {
+    //     ivec.push_back(num);
+    //     if (cin.peek() == '\n') {
+    //         cin.get();
+    //         break;
+    //     }
+    // }
+
+    // cout << "Show sum of adjacenet values: \n";
+    // for (decltype(ivec.size()) sz = 0;  sz < ivec.size(); sz=sz+2) {
+    //     if ((sz + 1) >= ivec.size())
+    //         break;        
+    //     cout << ivec[sz] << " + " << ivec[sz+1] << " = " << ivec[sz] + ivec[sz+1] << endl;
+    // }
+
+    // cout << "Show sum of first and last numbers and so on...: \n";
+    // for (decltype(ivec.size()) sz = 0;  sz < (ivec.size()-1-sz); sz++) {
+    //     cout << ivec[sz] << " + " << ivec[ivec.size()-1-sz] << " = " << ivec[sz] + ivec[ivec.size()-1-sz] << endl;
+
+    // }
+
+
 
     return 0;
 }
+
+// https://cpp-primer.pages.dev/book/032-3.3._library_vector_type.html
